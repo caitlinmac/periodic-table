d3c4eb23abf2899ad52f715242515cf67452c3f0
Caitlin M <caitlin.maceachen@gmail.com>
2024-04-04 18:28:43 +0000

    fix: pipe not piping

--- element.sh
+++ element.sh
@@ -24,22 +24,38 @@ else
     #if argument provided is atomic number
     if [[ $ARG_NO == $1 ]]
     then
-      ATOMIC_NO=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements FULL JOIN properties USING(atomic_number) FULL JOIN types USING(type_id) WHERE atomic_number=$1;")
-      echo "$ATOMIC_NO"
       ##FIX NEEDED: NOT PIPING INTO THE VARIABLES!
-      echo $ATOMIC_NO | read NUM BAR NAME BAR SYMBOL BAR TYPE BAR MASS BAR MELT BAR BOIL BAR TYPE 
-      echo $NUM $SYMBOL $NAME $MASS $MELT
+      ATOMIC_NO=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements FULL JOIN properties USING(atomic_number) FULL JOIN types USING(type_id) WHERE atomic_number=$1;") 
+       # Set IFS to tab character
+       IFS=$'|'
+      echo "$ATOMIC_NO" | while read NUM NAME SYMBOL TYPE MASS MELT BOIL
+      do
       echo "The element with atomic number $NUM is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
+      done
   #if argument provided is symbol
     elif [[ $ARG_SYM == $1 ]]
     then
       echo Symbol!
-      ATOMIC_NO=$($PSQL "SELECT atomic_number FROM elements WHERE symbol='$1';")
+      #ATOMIC_NO=$($PSQL "SELECT atomic_number FROM elements WHERE symbol='$1';")
+      ATOMIC_NO=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements FULL JOIN properties USING(atomic_number) FULL JOIN types USING(type_id) WHERE symbol=$1;") 
+       # Set IFS to tab character
+       IFS=$'|'
+      echo "$ATOMIC_NO" | while read NUM NAME SYMBOL TYPE MASS MELT BOIL
+      do
+      echo "The element with atomic number $NUM is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
+      done
   #if argument provided is name
     elif [[ $ARG_NAME == $1 ]]
     then
       echo Name!
-      ATOMIC_NO=$($PSQL "SELECT atomic_number FROM elements WHERE name='$1';")
+      #ATOMIC_NO=$($PSQL "SELECT atomic_number FROM elements WHERE name='$1';")
+      ATOMIC_NO=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements FULL JOIN properties USING(atomic_number) FULL JOIN types USING(type_id) WHERE name=$1;") 
+       # Set IFS to tab character
+       IFS=$'|'
+      echo "$ATOMIC_NO" | while read NUM NAME SYMBOL TYPE MASS MELT BOIL
+      do
+      echo "The element with atomic number $NUM is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
+      done
     fi
   fi
 fi
